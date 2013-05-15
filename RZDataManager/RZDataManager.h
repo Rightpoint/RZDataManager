108f2f3946e8d60aeb8c3b7ff76ed17ab410435a
Stephen Barnes <stephen.barnes@raizlabs.com>
2013-05-15 10:22:40 -0400

    Include CoreData.h for RZDataManager in case CoreData.h isn't included project wide

--- RZDataManager/RZDataManager.h
+++ RZDataManager/RZDataManager.h
@@ -6,6 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 
 typedef void (^RZDataManagerCompletionBlock)();
 typedef void (^RZDataManagerImportBlock)(NSManagedObjectContext* moc);
