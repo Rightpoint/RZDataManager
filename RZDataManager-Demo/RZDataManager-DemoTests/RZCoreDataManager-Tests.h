072886d9b0f8fa3b0651d6a5b65ba8f038e62569
Nick Donaldson <nick.donaldson@raizlabs.com>
2014-06-25 11:29:28 -0400

    Fix OCUnit deprecation warning

--- RZDataManager-Demo/RZDataManager-DemoTests/RZCoreDataManager-Tests.h
+++ RZDataManager-Demo/RZDataManager-DemoTests/RZCoreDataManager-Tests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Raizlabs. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface RZCoreDataManager_Tests : SenTestCase
+@interface RZCoreDataManager_Tests : XCTestCase
 
 @end
